52214a9c485a70a22f72bddac0cb29a6b1cba466
bot-maker <none>
2017-03-29 17:44:25 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,83 @@
-// Globals
-// newScript
-// getScript
-// addGreeting
-// request
+const trivia = {
+  'history': [ // this is a trivia topic
+    { q: 'Question 1', // this is the question we will ask the user
+      w: ['a2', 'a3', 'a4'], // these are wrong answers
+      c: 'a1', // this is the correct answer
+     },
+     { q: 'Question 2', w: ['a2', 'a3', 'a4'], c: 'a1' },
+     { q: 'Question 3', w: ['a2', 'a3', 'a4'], c: 'a1' },
+   ],
+   'sports': [
+    { q: 'Question 1', w: ['a2', 'a3', 'a4'], c: 'a1' },
+    { q: 'Question 2', w: ['a2', 'a3', 'a4'], c: 'a1' },
+    { q: 'Question 3', w: ['a2', 'a3', 'a4'], c: 'a1' },
+   ],
+};
+
+addGreeting((user, response) => {
+  response.sendText('Welcome to trivia time!');
+  user.score = 0; //set the user's score to 0
+});
+// after the greeting is sent the default script will be called
+
+newScript() // 
+  .dialog('start', (session, response, stop) => {
+    response.sendText('Pick a trivia topic');
+    const topics = Object.keys(trivia).join(' or ');
+    response.sendText(topics);
+  })
+  .expect.text((session, response, stop) => {
+    response.startScript(session.message.text);
+  });
+
+Object.keys(trivia).forEach((topic) => { // iterate through each topic
+  newScript(topic) // a new script with the topic name
+    /* 
+     * begin dialogs are like greetings for scripts, they are only sent once
+     */
+    .begin((session, response, stop) => { 
+      response.sendText(`Time to test you on ${topic}!`);
+      session.user.question_number = 0; // Reset what question the user is on
+    })
+    .intent.always('general', 'help', (session, response) => {
+      response.sendText(`You are in the ${topic} section`);
+      response.goto('start');
+    })
+    /* 
+     * If the dialog doesn't call stop() then the script will automatically flow 
+     * to the next dialog. We can also use named dialogs to move around a script
+     * we'll use this later to loop around the questions
+     */
+    .dialog('start', (session, response, stop) => {
+      const question = trivia[topic][session.user.question_number];
+      response
+        .sendText(question.q)  
+        .sendText('Is it:'); //notice how you can chain responses (but don't have to!)
+      response.sendText(question.w.concat(question.c).join(' or ')); //exercise for the reader to shuffle these!
+    })
+    /* 
+     * The script will stop here because the next dialog is an expect dialog, which tells
+     * alana to wait for input from the user. We can even specialize the expect to only 
+     * response to a text message.
+     */
+    .expect.text((session, response, stop) => {
+      const question = trivia[topic][session.user.question_number];
+      if (session.message.text === question.c) {
+        response.sendText('Correct!');
+        session.user.score++;
+      } else {
+        response.sendText(`Wrong :( it was ${question.c}`);
+        session.user.score = Math.max(0, session.user.score - 1);
+      }
+      response.sendText(`Your score is ${session.user.score}`);
+      session.user.question_number++;
+      if (session.user.question_number < trivia[topic].length) {
+        // we still have questions, ask the next one
+        response.goto('start');
+       }
+    });
+    /* 
+     * At the end of script, alana will automatically move the user to the default
+     * script, usually the main menu
+     */
+});
--- tests/main.js
+++ tests/main.js
@@ -1,8 +1,52 @@
-// Globals
-// test
-// newTest
-//
-// test('hello-world', function() {
-//   return newTest()
-//     .run();
-// });
+test('greeting', function(){
+  return newTest()
+    .expectText('Welcome to trivia time!')
+    .run();
+})
+
+test('menu', function(){
+  return newTest()
+    .expectText('Welcome to trivia time!')
+    .expectText('Pick a trivia topic')
+    .expectText('history or sports')
+    .run();
+})
+
+test('gameplay', function() {
+  return newTest()
+    .checkForTrailingDialogs()
+    .expectText('Welcome to trivia time!')
+    .expectText('Pick a trivia topic')
+    .expectText('history or sports')
+    .sendText('history')
+    .expectText('Time to test you on history!')
+    .expectText('Question 1')
+    .expectText('Is it:')
+    .expectText('a2 or a3 or a4 or a1')
+    .run();
+})
+
+test('intent', function() {
+  return newTest()
+    .checkForTrailingDialogs()
+    .expectText('Welcome to trivia time!')
+    .expectText('Pick a trivia topic')
+    .expectText('history or sports')
+    .sendText('history')
+    .expectText('Time to test you on history!')
+    .expectText('Question 1')
+    .expectText('Is it:')
+    .expectText('a2 or a3 or a4 or a1')
+    .sendText('a1')
+    .expectText('Correct!')
+    .expectText('Your score is 1')
+    .expectText('Question 2')
+    .expectText('Is it:')
+    .expectText('a2 or a3 or a4 or a1')
+    .sendText('help')
+    .expectText('You are in the history section')
+    .expectText('Question 2')
+    .expectText('Is it:')
+    .expectText('a2 or a3 or a4 or a1')
+    .run();
+})
